509c8d7ccee2d6b666d387bc30461efc7105ea17
hautran2705 <hautran2705@gmail.com>
2019-11-17 11:37:14 +0700

    + phân loại sản phẩm

--- routes/san-pham.js
+++ routes/san-pham.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product')
+var Category = require('../models/category')
+var async = require('async');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    const category = req.query.category;
-    if (category == null) {
+router.get('', async(req, res, next) => {
+    const id = req.query.loai;
+    if (id == null) {
         Product.find((err, callback) => {
             if (err)
                 res.sendStatus(404);
-            res.render('san-pham', { title: 'Sản phẩm - Tất cả', products: callback });
+            Category.find(function(err,cat){
+                if (!err) res.render('san-pham', { title: 'Sản phẩm - Tất cả', products: callback, categorys: cat });
+            });
         })
+    } else
+    {
+        let displayProd, rela;
+        const productFind = Category.findById(id);
+        displayProd = await productFind.exec();
+        const relaProductFind = Product.find({ category: displayProd.id});
+        rela = await relaProductFind.exec();
+        Category.find(function(err,cat){
+            if (!err) res.render('san-pham', { title: 'Sản phẩm - ' + displayProd.name, products: rela, categorys: cat });
+        });
     }
 });
+
 
 
 module.exports = router;
