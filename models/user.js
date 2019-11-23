7bf49db23254c331724c6674f16e970e997064bc
Hoang Pham <hoangpm6814@gmail.com>
2019-11-23 08:29:11 +0700

    ! form đăng ký

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var UsersSchema = new mongoose.Schema({
+    username: {
+		type: String,
+		default: "no_id"
+	},
+	email: {
+		type: String,
+		default: "a@gmail.com"
+	},
+	password: {
+		type:  String,
+		default: "0"
+	},
+	authen: {
+		type:  String,
+		default: "0"
+	}
+});
+var Users = new mongoose.model('users', UsersSchema);
+exports.user = Users;
--- routes/dang-ki.js
+++ routes/dang-ki.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('dang-ki', { title: 'Đăng kí' });
+router.get('/', function (req, res, next) {
+    res.render('dang-ki', {
+        error: ""
+    });
+});
+
+router.post('/', async function (req, res, next) {
+    if (req.body.username && req.body.email && req.body.password) {
+        let user = new db.user({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            authen: "1"
+        });
+        await user.save(async function (err, user_mini) {
+            if (err) {
+                console.log(err);
+                res.render('dang-ki', {
+                    error: ""
+                });
+            } else if (user_mini) {
+                res.redirect('/');
+            } else { }
+        });
+    } else {
+        res.render('dang-ki', {
+            error: 'Hãy nhập đầy đủ thông tin!'
+        })
+    }
 });
 
 module.exports = router;
