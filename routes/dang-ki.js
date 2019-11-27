2cdeb05c1016fa77ad7dc8d273193583a3c01122
Hoang Pham <hoangpm6814@gmail.com>
2019-11-27 20:47:04 +0700

    ! Dang ki feature ok

--- routes/dang-ki.js
+++ routes/dang-ki.js
@@ -9,28 +9,52 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.post('/', async function (req, res, next) {
+router.post('/', function (req, res, next) {
     if (req.body.username && req.body.email && req.body.password) {
-        let user = new db.user({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            authen: "1"
-        });
-        await user.save(async function (err, user_mini) {
-            if (err) {
-                console.log(err);
-                res.render('dang-ki', {
-                    error: ""
-                });
-            } else if (user_mini) {
-                res.redirect('/');
-            } else { }
-        });
+        console.log(req.body.password.length);
+        if (req.body.password.length < 7) 
+        {
+            res.render('dang-ki', {
+                error: 'Password ít nhất 7 kí tự!'
+            });
+        }
+        else 
+        {
+            let user = new db.user({
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                authen: "1"
+            });
+            db.user.countDocuments({username: req.body.username }, function (err, count) { 
+                console.log(count);
+                if (err) console.log("err");
+                if(count>0){
+                    //document exists
+                    res.render('dang-ki', {
+                        error: 'Username đã tồn tại!'
+                    });
+                }
+                else 
+                {
+                    console.log("Vo else");
+                    user.save(function (err, user_ok) {
+                        if (err) {
+                            console.log(err);
+                            res.render('dang-ki', {
+                                error: ""
+                            });
+                        } else if (user_ok) {
+                            res.redirect('/');
+                        } 
+                    });
+                }
+            });
+        }
     } else {
         res.render('dang-ki', {
             error: 'Hãy nhập đầy đủ thông tin!'
-        })
+        });
     }
 });
 
