e31dd7730049c075f242ac7109daf3dd91fe2aea
Hoang Pham <hoangpm6814@gmail.com>
2019-11-29 09:42:35 +0700

    ! Hash password register

--- routes/dang-ki.js
+++ routes/dang-ki.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,41 +14,63 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
     if (req.body.username && req.body.email && req.body.password) {
         console.log(req.body.password.length);
-        if (req.body.password.length < 7) 
-        {
+        if (req.body.password.length < 7) {
             res.render('dang-ki', {
                 error: 'Password ít nhất 7 kí tự!'
             });
         }
-        else 
-        {
+        else {
             let user = new db.user({
                 username: req.body.username,
                 email: req.body.email,
                 password: req.body.password,
                 authen: "1"
             });
-            db.user.countDocuments({username: req.body.username }, function (err, count) { 
+            db.user.countDocuments({ username: req.body.username }, function (err, count) {
                 console.log(count);
                 if (err) console.log("err");
-                if(count>0){
+                if (count > 0) {
                     //document exists
                     res.render('dang-ki', {
                         error: 'Username đã tồn tại!'
                     });
                 }
-                else 
-                {
+                else {
                     console.log("Vo else");
-                    user.save(function (err, user_ok) {
-                        if (err) {
-                            console.log(err);
-                            res.render('dang-ki', {
-                                error: ""
-                            });
-                        } else if (user_ok) {
-                            res.redirect('/');
-                        } 
+                    ///////////////////////////////////////////////////////
+                    // bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
+                    //     .then(function (hashedPassword) {
+                    //         return usersDB.saveUser(username, hashedPassword);
+                    //     })
+                    //     .then(function () {
+                    //         res.send();
+                    //     })
+                    //     .catch(function (error) {
+                    //         console.log("Error saving user: ");
+                    //         console.log(error);
+                    //         next();
+                    //     });
+                    /////////////////////////////////////////////////////////
+                    // user.save(function (err, user_ok) {
+                    //     if (err) {
+                    //         console.log(err);
+                    //         res.render('dang-ki', {
+                    //             error: ""
+                    //         });
+                    //     } else if (user_ok) {
+                    //         res.redirect('/');
+                    //     }
+                    // });
+                    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                        db.user.create({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash
+                        }).then(function (data) {
+                            if (data) {
+                                res.redirect('/');
+                            }
+                        });
                     });
                 }
             });
