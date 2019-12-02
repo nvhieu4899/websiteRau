bc89dce0fec40496ba055cffd4d4e75279759b0d
Hoang Pham <hoangpm6814@gmail.com>
2019-12-03 00:32:37 +0700

    Thêm thẳng passport vô đăng nhập

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/san-pham');
@@ -31,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/dang-nhap.js
+++ routes/dang-nhap.js
@@ -1,9 +1,34 @@
 var express = require('express');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 var router = express.Router();
 var db = require('../models/user');
 var passport = require('passport');
+
+
 passport.initialize();
-require('../passport/authenticate')(passport);
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
+            return done(null, user);
+        });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    // Coi lai cai nay ne... 
+    User.getUserById(id, function (err, user) {
+        done(err, user);
+    });
+});
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
