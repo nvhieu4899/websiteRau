2d1249b4158dff9ca7655382fba34daecb9d6857
Hoang Pham <hoangpm6814@gmail.com>
2019-12-04 20:42:23 +0700

    ! Đăng xuất

--- passport/passport.js
+++ passport/passport.js
@@ -18,7 +18,7 @@ passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(async function (id, done) {
+passport.deserializeUser(function (id, done) {
     User.findById(id, function(err, user) {
         done(err, user);
     });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var product = require('../models/product');
+var user = require("../models/user");
 /* GET home page. */
 router.get('/', (req, res, next) => {
     product.find((err, callback) => {
         res.render('index', { title: 'Rau - Rau sạch cho mọi nhà', products: callback, user: req.user });
     }).limit(8)
+}); 
+
+router.get("/dang-xuat", (req, res) => {
+    req.logout();
+    res.redirect("/");
 });
 module.exports = router;
