10662258aa2340c93805d2636c2e9ab9cfffbbb0
Nguyen Van Hieu <elpulga246@gmail.com>
2020-01-04 18:20:58 +0700

    + helpers handlebars, hiển thị trạng thái đã, đang, chưa giao dạng select

--- models/order.js
+++ models/order.js
@@ -7,8 +7,8 @@ var schema = new Schema({
     address: { type: String, required: true },
     name: { type: String, required: true },
     phone: { type: String, required: true },
-    date: { type: Date },
-    status: { type: Number }
+    date: { type: Date, default: new Date() },
+    status: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Order', schema);
